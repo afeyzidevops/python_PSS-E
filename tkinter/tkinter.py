cf0de31c5a23a60aee3dd19a7c8b3566ec805e28
afeyzidevops1 <mohandes.ali.feizi@gmail.com>
2024-05-27 09:03:21 +0330

    edit tkinter.py for fix error in line 24

--- tkinter/tkinter.py
+++ tkinter/tkinter.py
@@ -13,17 +13,18 @@ Aras_Label.pack()
 Aras_Label.config(fg="brown")
 
 ##################################################################################################################
-##                                                                                                              ##
+##
+Buttom_solve=Label(window,text="count: ")
+Buttom_solve.pack()
 counter=0
-def counter():
+def Counter():
    global counter
    counter +=1
    #print counter
-   Buttom_solve.config(text="count: %s "%counter)
+   Buttom_solve.config(text="count: %i"%counter)
    
-Button(window,text="Solve",command=counter ,font=('TITR',10),bg="yellow", fg="blue").pack()
-Buttom_solve=Label(window,text="count: ")
-Buttom_solve.pack()
+Button(window,text="Solve",command=Counter ,font=('TITR',10),bg="yellow", fg="blue").pack()
+
 ##                                                                                                              ##
 ##################################################################################################################
 
@@ -31,12 +32,12 @@ Buttom_solve.pack()
 
 ##################################################################################################################
 ##                                                                                                              ##
-open_sav_Label=Label(window,text="Aras Simlink asistance",foreground="red",font=("Titr",20),background="Green")
+open_sav_Label=Label(window,text="Please insert a *.sav file ",foreground="red",font=("Titr",20),background="Green")
 open_sav_Label.pack()
 
 def open_sav():
-   open_sav_Label.config(fg="green")
-   Buttom_solve.config(text="file opened: %s ", bg='green')
+   open_sav_Label.config(text="*.sav file inserted ",fg="green",bg="brown")
+   
 
 Button(window,text="open **.sav file",command=open_sav ,font=('TITR',10),bg="yellow", fg="blue").pack()
 ##                                                                                                              ##
