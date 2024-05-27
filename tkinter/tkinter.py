b664fc1774323c746a1166d81b202b95ee394ef1
afeyzidevops1 <mohandes.ali.feizi@gmail.com>
2024-05-27 11:51:55 +0330

    add Entry to Line 40 and print it in def open_save in line 43

--- tkinter/tkinter.py
+++ tkinter/tkinter.py
@@ -1,20 +1,20 @@
 from Tkinter import *
 
-window =Tk()
-window.title("welcom to PSS/E Asistant software")
+root =Tk()
+root.title("welcom to PSS/E Asistant software")
 
-#window.minsize(300,400)
-#window.maxsize(640,450)
-window.geometry("600x480")
-window.resizable(width=False, height=False)
+#root.minsize(300,400)
+#root.maxsize(640,450)
+root.geometry("900x750")
+root.resizable(width=False, height=False)
 
-Aras_Label=Label(window,text="Aras Simlink asistance",foreground="red",font=("Titr",20),background="Green")
+Aras_Label=Label(root,text="Aras Simlink asistance",foreground="red",font=("Titr",20),background="Green")
 Aras_Label.pack()
 Aras_Label.config(fg="brown")
 
 ##################################################################################################################
 ##
-Buttom_solve=Label(window,text="count: ")
+Buttom_solve=Label(root,text="count: ")
 Buttom_solve.pack()
 counter=0
 def Counter():
@@ -23,7 +23,7 @@ def Counter():
    #print counter
    Buttom_solve.config(text="count: %i"%counter)
    
-Button(window,text="Solve",command=Counter ,font=('TITR',10),bg="yellow", fg="blue").pack()
+Button(root,text="Solve",command=Counter ,font=('TITR',10),bg="yellow", fg="blue").pack()
 
 ##                                                                                                              ##
 ##################################################################################################################
@@ -32,15 +32,27 @@ Button(window,text="Solve",command=Counter ,font=('TITR',10),bg="yellow", fg="bl
 
 ##################################################################################################################
 ##                                                                                                              ##
-open_sav_Label=Label(window,text="Please insert a *.sav file ",foreground="red",font=("Titr",20),background="Green")
+frame=Frame(root , width=100,height=150 , bd=5)
+frame.pack()
+open_sav_Label=Label(root,text="Please insert a *.sav file ",foreground="red",font=("Titr",20),background="Green")
+open_sav_Label.place(x=10 , y=10, width=300,height=160)
 open_sav_Label.pack()
+Savfile=Entry(root)
 
+Savfile.pack()
 def open_sav():
-   open_sav_Label.config(text="*.sav file inserted ",fg="green",bg="brown")
+  open_sav_Label.config(text=Savfile.get(),fg="green",bg="brown")
    
 
-Button(window,text="open **.sav file",command=open_sav ,font=('TITR',10),bg="yellow", fg="blue").pack()
+Button(root,text="open **.sav file",command=open_sav ,font=('TITR',10),bg="yellow", fg="blue",bd="5").pack()
+
 ##                                                                                                              ##
 ##################################################################################################################
 
-window.mainloop()
+
+##################################################################################################################
+##                                                                                                              ##
+
+
+
+root.mainloop()
