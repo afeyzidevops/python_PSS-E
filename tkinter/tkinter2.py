8c80b68664822784630164819e2f96e55342e493
afeyzidevops <a.feyzi.devops@gmail.com>
2024-05-28 23:20:51 +0330

    Edit delete list item function by delete selected item

--- tkinter/tkinter2.py
+++ tkinter/tkinter2.py
@@ -1,6 +1,6 @@
-from Tkinter import *
+import Tkinter as tk
 
-root =Tk()
+root =tk.Tk()
 root.title("welcom to PSS/E Asistant software")
 
 root.geometry("1080x750")
@@ -10,13 +10,13 @@ root.resizable(width=False, height=False)
               ############## edit  text box and add List box  wijet to project##########
 ##################################################################################################################
 ####
-text=Text(root)
-text.pack(anchor=N)
+text=tk.Text(root)
+text.pack()
 
 #Listbox_Label=Label(root,font=10,text="Senarios",bg= "blue",fg="white")
 #Listbox_Label.pack(anchor=W)
-ListBox=Listbox(root,font=10)
-ListBox.pack(anchor=W)
+ListBox=tk.Listbox(root,font=10)
+ListBox.pack()
 ListBox.insert(1,"Senario_01")
 ListBox.insert(2,"Senario_02")
 ListBox.insert(3,"Senario_03")
@@ -27,26 +27,31 @@ ListBox.insert(6,"Senario_06")
 def LoadSenario():
    with open("Senario_01.text", "r") as F :
        data=F.read()    
-       text.insert(INSERT,data)
-Button(root,text="Open",command=LoadSenario).pack()
+       text.insert(tk.INSERT,data)
+tk.Button(root,text="Open",command=LoadSenario).pack()
 #------------------------------------------------------------------------------SaveSenario
 def SaveSenario():
    with open ("Senario_01.text", "w") as F:
-      F.write(text.get(1.0,END))
+      F.write(text.get(1.0,tk.END))
       F.close()
-Button(root,text="Save senario",command=SaveSenario).pack(anchor=W)
+tk.Button(root,text="Save senario",command=SaveSenario).pack()
 #------------------------------------------------------------------------------AddSenario          
 def AddSenario ():
    with open("Senario_01.text") as S :
       Senario=S.read()
-      ListBox.insert(END,Senario)
-Button(root,text="Add senario",command=AddSenario).pack(anchor=W)
+      ListBox.insert(tk.END,Senario)
+tk.Button(root,text="Add senario",command=AddSenario).pack()
 #------------------------------------------------------------------------------ExequteSenario       
 def ExequteSenario ():
    with open("Senario1","r") as S :
       Senario=S.read()
-      text.insert(INSERT,Senario)      
-Button(root,text="Execute Senario",command=ExequteSenario).pack(anchor=W)
+      text.insert(tk.INSERT,Senario)      
+tk.Button(root,text="Execute Senario",command=ExequteSenario).pack()
+#------------------------------------------------------------------------------ExequteSenario       
+def DeleteSenario ():
+      ListBox.delete(tk.ANCHOR)
+      #ListBox.delete(END,1)      
+tk.Button(root,text="Delete Senario",command=DeleteSenario).pack()
 ##                                                                                                              ##
 ##################################################################################################################
 
