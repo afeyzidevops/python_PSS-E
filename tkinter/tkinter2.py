8684bc14c30ec1532deed71a385d966328ec025d
afeyzidevops1 <mohandes.ali.feizi@gmail.com>
2024-05-29 13:21:16 +0330

    add toplevel window for senario list and insert button to it

--- tkinter/tkinter2.py
+++ tkinter/tkinter2.py
@@ -23,18 +23,8 @@ ListBox.insert(3,"Senario_03")
 ListBox.insert(4,"Senario_04")
 ListBox.insert(5,"Senario_05")
 ListBox.insert(6,"Senario_06")
-#------------------------------------------------------------------------------LoadSenario
-def LoadSenario():
-   with open("Senario_01.text", "r") as F :
-       data=F.read()    
-       text.insert(tk.INSERT,data)
-tk.Button(root,text="Open",command=LoadSenario).pack()
-#------------------------------------------------------------------------------SaveSenario
-def SaveSenario():
-   with open ("Senario_01.text", "w") as F:
-      F.write(text.get(1.0,tk.END))
-      F.close()
-tk.Button(root,text="Save senario",command=SaveSenario).pack()
+
+
 #------------------------------------------------------------------------------AddSenario          
 def AddSenario ():
    with open("Senario_01.text") as S :
@@ -50,9 +40,64 @@ tk.Button(root,text="Execute Senario",command=ExequteSenario).pack()
 #------------------------------------------------------------------------------ExequteSenario       
 def DeleteSenario ():
       ListBox.delete(tk.ANCHOR)
-      #ListBox.delete(END,1)      
+          
 tk.Button(root,text="Delete Senario",command=DeleteSenario).pack()
 ##                                                                                                              ##
 ##################################################################################################################
+
+
+              ############## add top level window by insert text box and scroll bar in it ##########
+##################################################################################################################
+####
+
+def topLevel_1():
+   
+   top1=tk.Toplevel()
+   top1.geometry("800x900")
+   top1.title("Senario")
+   label_1=tk.Label(top1,text="Senario_01") 
+   text_1=tk.Text(top1,bg="Yellow")
+   Scrollbar=tk.Scrollbar(top1)
+   Scrollbar.config(command=text_1.yview)
+   Scrollbar.pack(side=tk.RIGHT,fill=tk.BOTH)
+   #for line in range(200):
+    #  text_1.insert(tk.INSERT,"this is a text   " + str(line) + "\n")
+#------------------------------------------------------------------------------OpenSenario
+   def Open_senario():
+      with open("Senario_01.text", "r") as F :
+         data=F.read()    
+         text_1.insert(tk.INSERT,data)
+   Open_Senario=tk.Button(top1,text="Open Senario",command=Open_senario)
+#------------------------------------------------------------------------------SaveSenario
+   def SaveSenario():
+      with open ("Senario_01.text", "w") as F:
+         F.write(text_1.get(1.0,tk.END))
+         F.close()
+   Save_btn=tk.Button(top1,text="Save senario",command=SaveSenario)
+
+   
+   Close_btn=tk.Button(top1,text="Close",command= top1.destroy)
+   
+   label_1.pack
+   text_1.pack(expand=True,fill=tk.BOTH)
+   Open_Senario.pack()
+   Save_btn.pack()
+   Close_btn.pack()
+   
+
+   
+   top1.mainloop()
+   
+#------------------------------------------------------------------------------LoadSenario
+
+tk.Button(root,text="Load Senario",command=topLevel_1).pack()   
+
+
+
+
+
+##                                                                                                              ##
+##################################################################################################################
+
 
 root.mainloop()
